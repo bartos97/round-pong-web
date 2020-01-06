3002ae7da460459de6fe0a10e4a1bf5dba732c94
Bartłomiej Zając <bartoss97@gmail.com>
2020-01-06 18:22:45 +0100

    Made game-screen template

--- src/app/components/home-screen/home-screen.controller.ts
+++ src/app/components/home-screen/home-screen.controller.ts
@@ -3,13 +3,13 @@ import { ComponentController } from '../../core/component-controller';
 export class HomeScreenController extends ComponentController {
     constructor(templateHook: HTMLElement) {
         super(templateHook);
-        this.regiterEvent(this.getElement('button'), 'click', this.onClick);
+        this.regiterEvent(this.getElement('#startButton'), 'click', this.buttonOnClick);
     }
 
-    private onClick(): void {
+    private buttonOnClick(): void {
         const game = document.getElementById('game-screen');
         this.TEMPLATE.hidden = true;
         game.hidden = false;
-        game.classList.add('show');
+        // game.classList.add('show');
     }
 }
