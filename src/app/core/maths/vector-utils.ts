6fdbcf39c14586c3dc49b5eb889e55494450657e
Bartłomiej Zając <bartoss97@gmail.com>
2020-01-09 21:01:47 +0100

    Implemented vector maths

--- src/app/core/maths/vector-utils.ts
+++ src/app/core/maths/vector-utils.ts
@@ -1,8 +1,27 @@
 import { Vector2D } from "./vector2D";
 
 export class VectorUtils {
-    // public static dot(vec: Vector2D): number {}
-    // public static normalize(vec: Vector2D): Vector2D {}
-    // public static distance(vecFrom: Vector2D, vecTo: Vector2D): number {}
-    // public static reflect(unitVector: Vector2D, normal: Vector2D): Vector2D {}
+    public static vectorLength(vec: Vector2D): number {
+        return Math.sqrt(vec.x * vec.x + vec.y * vec.y);
+    }
+
+    public static dot(vec1: Vector2D, vec2: Vector2D): number {
+        return vec1.x * vec2.x + vec1.y * vec2.y;
+    }
+
+    public static normalize(vec: Vector2D): Vector2D {
+        return new Vector2D(vec.x / vec.length,  vec.y / vec.length);
+    }
+
+    public static distance(pointFrom: Vector2D, pointTo: Vector2D): number {
+        return this.vectorLength(pointFrom.subtract(pointTo));
+    }
+
+    public static reflect(incidentUnitVector: Vector2D, normal: Vector2D): Vector2D {
+        return incidentUnitVector.subtract(
+            normal.multiply(2 * this.dot(normal, incidentUnitVector))
+        );
+    }
+
+    private constructor(){}
 }
--- src/app/core/maths/vector2D.ts
+++ src/app/core/maths/vector2D.ts
@@ -1,8 +1,51 @@
+import { VectorUtils } from "./vector-utils";
+
 export class Vector2D {
     public x: number;
     public y: number;
 
-    // public multiply(vec: Vector2D): Vector2D {}
-    // pubic add(vec: Vector2D): Vector2D {}
-    // public subtract(vec: Vector2D): Vector2D {}
+    public get length(): number {
+        return VectorUtils.vectorLength(this);
+    }
+
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public multiply(vec: Vector2D): Vector2D;
+    public multiply(val: number): Vector2D;
+    public multiply(x: any): Vector2D {
+        if (typeof x == "object") {
+            return new Vector2D(this.x * x.x, this.y * x.y);
+        }
+
+        if (typeof x == "number") {
+            return new Vector2D(this.x * x, this.y * x);
+        }
+    }     
+
+    public add(vec: Vector2D): Vector2D;
+    public add(val: number): Vector2D;
+    public add(x: any): Vector2D  {
+        if (typeof x == "object") {
+            return new Vector2D (this.x + x.x, this.y + x.y);
+        }
+
+        if (typeof x == "number") {
+            return new Vector2D (this.x + x, this.y + x);
+        }        
+    }
+
+    public subtract(vec: Vector2D): Vector2D;
+    public subtract(val: number): Vector2D;
+    public subtract(x: any): Vector2D  {
+        if (typeof x == "object") {
+            return new Vector2D (this.x - x.x, this.y - x.y);
+        }
+
+        if (typeof x == "number") {
+            return new Vector2D (this.x - x, this.y - x);
+        }        
+    }
 }
